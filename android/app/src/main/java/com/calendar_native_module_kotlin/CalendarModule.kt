9c3b739c7338d6ee12bfd10e6e174fa62a8fa0a5
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2022-03-23 01:16:12 -0300

    Open intent throug kotlin module

--- android/app/src/main/java/com/calendar_native_module_kotlin/CalendarModule.kt
+++ android/app/src/main/java/com/calendar_native_module_kotlin/CalendarModule.kt
@@ -1,5 +1,8 @@
 package com.calendar_native_module_kotlin
 
+import android.content.Intent
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Events.*
 import android.util.Log
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
@@ -14,7 +17,16 @@ class CalendarModule(reactContext: ReactApplicationContext) : ReactContextBaseJa
 
     @ReactMethod
     fun createCalendarEvent(name: String, location: String) {
-        Log.d("CalendarModule", "Create event called with name: " + name
+        Log.d("CalendarModuleKotlin", "Create event called with name: " + name
                 + " and location: " + location);
+        val intent = Intent(Intent.ACTION_INSERT)
+            .setData(CONTENT_URI)
+            .putExtra(TITLE, "Kotlin")
+            .putExtra(EVENT_LOCATION, "Remote")
+            .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+            .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + (60 * 60 * 1000))
+            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        context.startActivity(intent)
     }
 }
